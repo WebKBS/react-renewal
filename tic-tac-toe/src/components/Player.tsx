068ebcfe3962b09db602dbd0b45f68fb18f45fc0
gangbeomsu <kbs21c1987@gmail.com>
2023-10-25 19:40:28 +0900

    콜백으로 상태를 관리해야한다.

--- tic-tac-toe/src/components/Player.tsx
+++ tic-tac-toe/src/components/Player.tsx
@@ -7,15 +7,16 @@ interface PlayerProps {
 
 export default function Player({ name, symbol }: PlayerProps) {
   const [isEditing, setIsEditing] = useState(false);
+  // const [changeInput, setChangeInput] = useState(name);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing); // 편집된 상태에서 최신상태를 얻는다. (모범사례)
   }
 
   let playerName = <span className="player-name">{name}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
   }
 
   return (
@@ -24,7 +25,7 @@ export default function Player({ name, symbol }: PlayerProps) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
