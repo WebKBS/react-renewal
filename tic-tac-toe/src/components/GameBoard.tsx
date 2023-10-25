4f4b7fde88a5d65dfed338a294965ba65e5c4485
gangbeomsu <kbs21c1987@gmail.com>
2023-10-25 20:42:26 +0900

    주석 추가

--- tic-tac-toe/src/components/GameBoard.tsx
+++ tic-tac-toe/src/components/GameBoard.tsx
@@ -11,9 +11,12 @@ export default function GameBoard() {
 
   function handleSelectSquare(rowIndex: number, colIndex: number) {
     setGameBoard((prevGameBoard) => {
+      // 상태가 변경될때 마다 원본 배열을 복사해서 새로 할당한다.
       const updatedBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
+
+      console.log(updatedBoard);
 
       updatedBoard[rowIndex][colIndex] = "X";
       return updatedBoard;
