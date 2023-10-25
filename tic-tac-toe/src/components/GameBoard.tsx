2ba34ef87318860df5c1422e4fe5792f11a70dd2
gangbeomsu <kbs21c1987@gmail.com>
2023-10-25 20:40:32 +0900

    버튼 클릭 로직 만들기

--- tic-tac-toe/src/components/GameBoard.tsx
+++ tic-tac-toe/src/components/GameBoard.tsx
@@ -1,18 +1,35 @@
-const initialGameBoard = [
+import { useState } from "react";
+
+const initialGameBoard: (string | null)[][] = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex: number, colIndex: number) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIdx) => (
+      {gameBoard.map((row, rowIdx) => (
         <li key={rowIdx}>
           <ol>
             {row.map((playerSymbol, colIdx) => (
               <li key={colIdx}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIdx, colIdx)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
