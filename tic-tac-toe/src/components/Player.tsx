494970432102ec8e10a53dc41789fa46a8dd56c8
gangbeomsu <kbs21c1987@gmail.com>
2023-10-25 19:21:59 +0900

    edit 버튼 조건걸기

--- tic-tac-toe/src/components/Player.tsx
+++ tic-tac-toe/src/components/Player.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 interface PlayerProps {
   name: string;
   symbol: string;
 }
 
 export default function Player({ name, symbol }: PlayerProps) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit2d</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
