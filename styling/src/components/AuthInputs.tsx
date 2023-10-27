616fcc8b3acc85cc59c5b661dfe5a4b8a613b07c
gangbeomsu <kbs21c1987@gmail.com>
2023-10-27 20:06:07 +0900

    조건부 styled-components 사용해보기. ($ 반드시 붙이기!!)

--- styling/src/components/AuthInputs.tsx
+++ styling/src/components/AuthInputs.tsx
@@ -8,23 +8,23 @@ const ControlContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const Label = styled.label`
+const Label = styled.label<{ $invalid: boolean }>`
   display: block;
   margin-bottom: 0.5rem;
   font-size: 0.75rem;
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
 `;
 
-const Input = styled.input`
+const Input = styled.input<{ $invalid: boolean }>`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
@@ -54,22 +54,21 @@ export default function AuthInputs() {
       <ControlContainer>
         <div className="paragraph">
           <p>
-            <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
-              Email
-            </Label>
+            <Label $invalid={emailNotValid}>Email</Label>
             <Input
               type="email"
+              $invalid={emailNotValid}
               // className={emailNotValid ? "invalid" : undefined}
-              style={{ backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db" }}
               onChange={(event) =>
                 handleInputChange("email", event.target.value)
               }
             />
           </p>
           <p>
-            <Label>Password</Label>
+            <Label $invalid={emailNotValid}>Password</Label>
             <Input
               type="password"
+              $invalid={emailNotValid}
               className={passwordNotValid ? "invalid" : undefined}
               onChange={(event) =>
                 handleInputChange("password", event.target.value)
