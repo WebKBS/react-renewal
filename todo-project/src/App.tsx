699e361b77f7586b438908aef741fdbc1ddd3a22
gangbeomsu <kbs21c1987@gmail.com>
2023-10-29 17:53:40 +0900

    조건부에 따라 프로젝트 등록 컴포넌트 변경 만들기

--- todo-project/src/App.tsx
+++ todo-project/src/App.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+
+interface ProjectData {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [projectsState, setProjectsState] = useState<{
+    selectedProjectId: null | undefined;
+    project: ProjectData[];
+  }>({
+    selectedProjectId: undefined,
+    project: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <ProjectsSidebar />
-        <NoProjectSelected />
+        <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+        {content}
       </main>
     </>
   );
--- todo-project/src/components/Button.tsx
+++ todo-project/src/components/Button.tsx
@@ -1,9 +1,9 @@
-export default function Button({
-  children,
-  ...props
-}: {
+interface ButtonType {
   children: React.ReactNode;
-}) {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export default function Button({ children, ...props }: ButtonType) {
   return (
     <button
       className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
