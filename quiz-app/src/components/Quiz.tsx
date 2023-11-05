21ad7ad08d8568bcd2d8c0c9898c4e3d41c05208
gangbeomsu <kbs21c1987@gmail.com>
2023-11-05 18:55:41 +0900

    퀴즈 모두 클릭후 완성 로직 만들기

--- quiz-app/src/components/Quiz.tsx
+++ quiz-app/src/components/Quiz.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizeCompleteImg from "../assets/quiz-complete.png";
 
 // interface QuestionType {
 //   id: string;
@@ -12,17 +13,33 @@ function Quiz() {
 
   const activeQuestionIndex = userAnswers.length;
 
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
   function handleSelectAnswer(selectedAnswer: string) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, [selectedAnswer]];
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizeCompleteImg} alt="Tropy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  // 이 로직은 quizIsComplete 이후에 실행 되어야한다.
+  const shuffedAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  // 랜덤으로 섞기 위한 함수
+  shuffedAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
       <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+        {shuffedAnswers.map((answer) => {
           return (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
