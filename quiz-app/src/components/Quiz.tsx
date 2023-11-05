c1ef4928cc199a20ffe27a95792c5a3dd1a91a20
gangbeomsu <kbs21c1987@gmail.com>
2023-11-05 19:25:35 +0900

    useCallback으로 종속성 전달하기

--- quiz-app/src/components/Quiz.tsx
+++ quiz-app/src/components/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import QUESTIONS from "../questions";
 import quizeCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer";
@@ -10,11 +10,18 @@ function Quiz() {
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer: string) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer: string
+  ) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, [selectedAnswer]];
     });
-  }
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(() => {
+    handleSelectAnswer("");
+  }, [handleSelectAnswer]);
 
   if (quizIsComplete) {
     return (
@@ -33,12 +40,7 @@ function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer
-          timeout={10000}
-          onTimeout={() => {
-            handleSelectAnswer(null);
-          }}
-        />
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffedAnswers.map((answer) => {
