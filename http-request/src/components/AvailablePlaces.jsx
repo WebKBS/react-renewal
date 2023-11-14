0bc49203889a3709aadcf96ca0861cf2467072a2
gangbeomsu <kbs21c1987@gmail.com>
2023-11-14 22:10:49 +0900

    파일 분리 및 에러처리

--- http-request/src/components/AvailablePlaces.jsx
+++ http-request/src/components/AvailablePlaces.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -11,19 +13,22 @@ export default function AvailablePlaces({ onSelectPlace }) {
     setIsFetching(true);
     async function fetchPlaces() {
       try {
-        const response = await fetch("http://localhost:3000/placs");
-        const resData = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if (!response.ok) {
-          throw new Error("데이터 페칭 실패");
-        }
-
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         console.log(error);
         setError({ message: error.message || "데이터 페치가 실패했습니다." });
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
 
     fetchPlaces();
