0f80bd81e10df52525b19d12264724a6c58d7131
강범수 <kbs21c1987@gmail.com>
2023-11-15 18:28:28 +0900

    Put 구현하기

--- http-request/src/http.js
+++ http-request/src/http.js
@@ -1,9 +1,24 @@
 export async function fetchAvailablePlaces() {
-  const response = await fetch("http://localhost:3000/places");
+  const response = await fetch('http://localhost:3000/places');
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error("데이터 페칭 실패");
+    throw new Error('데이터 페칭 실패');
   }
   return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error('PUT 데이터 페칭 실패');
+  }
+  return resData.message;
 }
