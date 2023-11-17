dc2bc7b14589501005d15293bae912da558cc408
강범수 <kbs21c1987@gmail.com>
2023-11-17 14:05:54 +0900

    blur를 사용해서 유효성검사

--- form-input/src/components/StateLogin.jsx
+++ form-input/src/components/StateLogin.jsx
@@ -9,6 +9,13 @@ export default function Login() {
     password: '',
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -19,6 +26,18 @@ export default function Login() {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: event,
+    }));
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
     }));
   }
 
@@ -41,9 +60,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>이메일을 정확하게 입력하세요.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -52,6 +75,7 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
