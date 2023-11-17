6e79c3447db352454021b1539bb188860cb8bf16
강범수 <kbs21c1987@gmail.com>
2023-11-17 11:17:54 +0900

    StateLogin 페이지 수정

--- form-input/src/components/StateLogin.jsx
+++ form-input/src/components/StateLogin.jsx
@@ -1,4 +1,6 @@
-function StateLogin() {
+import { useState } from 'react';
+
+export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [enteredPassword, setEnteredPassword] = useState('');
 
@@ -6,6 +8,13 @@ function StateLogin() {
     email: '',
     password: '',
   });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(enteredValues);
+  }
+
   function handleInputChange(identifier, event) {
     setEnteredValues((prevValues) => ({
       ...prevValues,
@@ -21,7 +30,39 @@ function StateLogin() {
   //   setEnteredPassword(event.target.value);
   // }
 
-  return <div>StateLogin</div>;
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
 
-export default StateLogin;
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
