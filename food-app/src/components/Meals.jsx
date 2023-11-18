4b8f2a9f749f064b5fe49c14e7d521b73b31cf2e
gangbeomsu <kbs21c1987@gmail.com>
2023-11-18 16:38:02 +0900

    1차 데이터 가져오기

---
+++ food-app/src/components/Meals.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        throw new Error(`${response.message} (${response.status})`);
+      }
+
+      const meals = await response.json();
+      console.log(response);
+      setLoadedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
