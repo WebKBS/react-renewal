2bbedf3b9dad3feadea35486b20fd01632d4ce07
gangbeomsu <kbs21c1987@gmail.com>
2023-11-19 17:17:37 +0900

    Modal 컴포넌트 만들기

--- food-app/src/components/Header.jsx
+++ food-app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
-import Button from "./Button";
+import Button from "../UI/Button";
 import CartContext from "../store/CartContext";
 
 export default function Header() {
