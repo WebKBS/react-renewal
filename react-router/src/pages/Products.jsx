f4c94406688d3146728c6ba3ef3cb27e68c880d5
gangbeomsu <kbs21c1987@gmail.com>
2023-11-25 16:44:56 +0900

    다이나믹 라우팅 Link연결하기

--- react-router/src/pages/Products.jsx
+++ react-router/src/pages/Products.jsx
@@ -1,11 +1,23 @@
+import { Link } from 'react-router-dom';
+
+const PRODUCTS = [
+  { id: 'p1', title: 'Product 1' },
+  { id: 'p2', title: 'Product 2' },
+  { id: 'p3', title: 'Product 3' },
+];
+
 export default function ProductsPage() {
   return (
     <>
       <h1>ProductsPage</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        {PRODUCTS.map((product) => (
+          <li>
+            <Link key={product.id} to={`/products/${product.id}`}>
+              {product.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
