f87d997f161130c5c30320f90763b0d12032db90
gangbeomsu <kbs21c1987@gmail.com>
2023-11-25 16:32:20 +0900

    useNavigate 사용해보기

--- react-router/src/pages/Home.jsx
+++ react-router/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products');
+  }
   return (
     <>
       <h1>HomePage</h1>
       <p>
         Go to <Link to="/Products">Products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
