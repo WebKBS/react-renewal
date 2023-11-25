16879d26d14eacd45a416664caabe1173d101d8c
gangbeomsu <kbs21c1987@gmail.com>
2023-11-25 18:02:31 +0900

    라우터 설정

--- router-backend/frontend/src/App.jsx
+++ router-backend/frontend/src/App.jsx
@@ -20,8 +20,23 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import EditEventPage from './pages/EditEvent';
+import EventDetailPage from './pages/EventDetail';
+import EventsPage from './pages/Events';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/events', element: <EventsPage /> },
+  { path: '/events/:eventId', element: <EventDetailPage /> },
+  { path: '/events/new', element: <NewEventPage /> },
+  { path: '/events/:eventId/edit', element: <EditEventPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
