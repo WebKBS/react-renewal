54220f1bd0762bbe0f728bb964f20cd4ed3f74a8
gangbeomsu <kbs21c1987@gmail.com>
2023-11-26 19:30:35 +0900

    에러 처리하기 또다른 방법

--- router-backend/frontend/src/pages/Error.jsx
+++ router-backend/frontend/src/pages/Error.jsx
@@ -1,3 +1,27 @@
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+import PageContent from './PageContent';
+
 export default function Error() {
-  return <h1>Error</h1>;
+  const error = useRouteError();
+  console.log(error);
+  let message = 'Something went wrong!';
+  let title = 'An error occured!';
+  if (error.status === 500) {
+    message = JSON.parse(error.data.message);
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = error.data.message;
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
--- router-backend/frontend/src/pages/Events.jsx
+++ router-backend/frontend/src/pages/Events.jsx
@@ -20,11 +20,14 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/evnts');
+  const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return { isError: true, message: 'Something went wrong!' };
-    throw new Error('Something went wrong!');
+    // throw new Error('Something went wrong!');
+    throw new Response(JSON.stringify({ message: 'Something went wrong!' }), {
+      status: 500,
+    });
   } else {
     const resData = await response.json();
     return resData.events;
