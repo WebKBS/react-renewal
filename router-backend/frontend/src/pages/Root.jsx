aa63683b7a96aa1025f3a1e0506ba6027e0f671e
gangbeomsu <kbs21c1987@gmail.com>
2023-11-26 19:07:23 +0900

    navigation으로 loading 미리 보여주기

--- router-backend/frontend/src/pages/Root.jsx
+++ router-backend/frontend/src/pages/Root.jsx
@@ -1,14 +1,18 @@
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
 export default function RootLayout() {
-  const events = useLoaderData();
-  console.log(events);
+  // const events = useLoaderData();
+
+  // console.log(events);
+
+  const navigation = useNavigation();
 
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === 'loading' && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
